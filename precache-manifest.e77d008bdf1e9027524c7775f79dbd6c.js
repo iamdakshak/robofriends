4ac0c193e712dc318b432ffad3900f9f56e22c7e
Dakshak Nagrale <dakshak1997@rediffmail.com>
2020-07-21 03:04:05 +0530

    Updates

---
+++ precache-manifest.e77d008bdf1e9027524c7775f79dbd6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae10c35612d4b699b3af05aaa8332d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4113585b6977d96f043d",
+    "url": "/static/css/main.490f2765.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4113585b6977d96f043d",
+    "url": "/static/js/main.39bc14cd.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
