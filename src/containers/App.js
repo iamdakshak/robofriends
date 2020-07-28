908442b64485ec46c4196c0c2f315917b11e7416
Dakshak Nagrale <dakshak1997@rediffmail.com>
2020-07-29 02:25:15 +0530

    Adding ErrorBoundary() for handling errors for CardList

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css'
 import Scroll from '../components/Scroll'
 
@@ -36,7 +37,9 @@ class App extends Component{
           <h1 className='grow bw2 f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
-            <CardList robots={ filteredRobots }/>
+            <ErrorBoundry>
+              <CardList robots={ filteredRobots }/>
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
